eddb756048c330f392d9a515cebe5467867244c6
Ha Rim Ku <hrku19@gmail.com>
2020-08-19 13:56:41 -0700

    Install new dependencies & Reformat

--- models/partner.js
+++ models/partner.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
+/** @format */
+
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-require('mongoose-currency').loadType(mongoose);
+require("mongoose-currency").loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const partnerSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    featured: {
-        type: Boolean,
-        default: false
-    },
-    description: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
+const partnerSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		featured: {
+			type: Boolean,
+			default: false,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-const Partner = mongoose.model('Partner', partnerSchema);
+const Partner = mongoose.model("Partner", partnerSchema);
 
 module.exports = Partner;
